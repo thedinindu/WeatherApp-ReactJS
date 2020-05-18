8d398760f922da7a3a2a01d628dd960e1aa0c26c
thedinindu <thedinindu@gmail.com>
2020-05-19 01:21:42 +0530

    Updates

---
+++ precache-manifest.8517991b9bddb77bb679667e523f5d04.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79a2620ceec10c5ba87ba21cd4d4980",
+    "url": "/WeatherApp-ReactJS/index.html"
+  },
+  {
+    "revision": "41186341a99b301d93e3",
+    "url": "/WeatherApp-ReactJS/static/css/2.22362eaa.chunk.css"
+  },
+  {
+    "revision": "dad74f723ea132068569",
+    "url": "/WeatherApp-ReactJS/static/css/main.fbd86ffc.chunk.css"
+  },
+  {
+    "revision": "41186341a99b301d93e3",
+    "url": "/WeatherApp-ReactJS/static/js/2.e8ba7771.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp-ReactJS/static/js/2.e8ba7771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dad74f723ea132068569",
+    "url": "/WeatherApp-ReactJS/static/js/main.fedf2a9b.chunk.js"
+  },
+  {
+    "revision": "99a6464eec079b026ec7",
+    "url": "/WeatherApp-ReactJS/static/js/runtime-main.0814dc55.js"
+  },
+  {
+    "revision": "9c44734c723d8d40ad814396eff99866",
+    "url": "/WeatherApp-ReactJS/static/media/bg.9c44734c.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/WeatherApp-ReactJS/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/WeatherApp-ReactJS/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/WeatherApp-ReactJS/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/WeatherApp-ReactJS/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/WeatherApp-ReactJS/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
